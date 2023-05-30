638c2d5cbc013cfff4e373244bfa759a960808be
duszaemilia <dusza.emilia@gmail.com>
2023-05-30 12:19:21 +0200

    fetch and myFetch custom implementation  with HTMLRequest

--- app.js
+++ app.js
@@ -1,46 +1,99 @@
 const boxRef = document.querySelector('.box');
 
 
-let isFinished = false;
+// let isFinished = false;
+//
+// //calbackhell
+// setTimeout(() => {
+//     boxRef.style.left = '500px';
+//     setTimeout(() => {
+//         boxRef.style.top = '500px';
+//         setTimeout(() => {
+//             boxRef.style.left = '0px';
+//             setTimeout(() => {
+//                 boxRef.style.top = '0px';
+//                 isFinished = true;
+//             }, 3000);
+//         }, 3000);
+//     }, 3000);
+// }, 3000);
+//
+//
+// setTimeout(() => {
+//
+//     if (isFinished) {
+//         console.log('run-again');
+//     }
+// }, 15000);
+//
+//
+// const receipt = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('kebab maj friend');
+//     }, 3001);
+//     setTimeout(() => {
+//         reject('no kebab, łonna fajt?')
+//     }, 3000);
+// });
+//
+// console.log(receipt);
+// receipt
+//     .then((msg) => {
+//         console.log(msg)
+//     })
+//     .catch((msg) => {
+//         console.log(msg)
+//     });
 
-//calbackhell
-setTimeout(() => {
-    boxRef.style.left = '500px';
-    setTimeout(() => {
-        boxRef.style.top = '500px';
-        setTimeout(() => {
-            boxRef.style.left = '0px';
-            setTimeout(() => {
-                boxRef.style.top = '0px';
-                isFinished = true;
-            }, 3000);
-        }, 3000);
-    }, 3000);
-}, 3000);
+// function move(top, left) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             boxRef.style.left = left;
+//             boxRef.style.top = top;
+//             resolve();
+//         }, 3000);
+//     });
+// }
+//
+// move(0, '500px')
+//     .then(() => move ('500px', '500px'))
+//     .then(() => move ('500px', 0))
+//     .then(() => move (0, 0));
+
+const url = 'https://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json';
+//
+// const request = fetch(url);
+//
+// request
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.rates[0].mid))
+//     .catch(console.log);
 
 
-setTimeout(() => {
+function myFetch(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
 
-    if (isFinished) {
-        console.log('run-again');
-    }
-}, 15000);
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 400) {
+                const response = JSON.parse(xhr.responseText)
+                resolve(response);
+            } else {
+                reject(xhr.status);
+            }
+        }
 
-
-const receipt = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('kebab maj friend');
-    }, 3001);
-    setTimeout(() => {
-        reject('no kebab, łonna fajt?')
-    }, 3000);
-});
-
-console.log(receipt);
-receipt
-    .then((msg) => {
-        console.log(msg)
+        xhr.onerror = function () {
+            reject('sth is no yes')
+        }
+        xhr.open('GET', url, true);
+        xhr.send()
     })
-    .catch((msg) => {
-        console.log(msg)
-    });
+}
+
+const request = myFetch(url);
+
+request
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+    .finally(() => {console.log('done')})
